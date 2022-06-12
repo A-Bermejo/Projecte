1ec76338dd208a091b4fdc907499b2ba5c542b00
A-Bermejo <a.bermejo@sapalomera.cat>
2022-06-12 16:19:15 +0200

    heroku

--- src/databaseConfig.js
+++ src/databaseConfig.js
@@ -1,18 +1,18 @@
-module.exports = {
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'what_the_food'
-    }
-}
-
-// HEROKU
 // module.exports = {
 //     database: {
-//         host: 'eu-cdbr-west-02.cleardb.net',
-//         user: 'b059417312a2a9',
-//         password: 'a9403b41',
-//         database: 'heroku_73fc860b4229c8a'
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'what_the_food'
 //     }
 // }
+
+HEROKU
+module.exports = {
+    database: {
+        host: 'eu-cdbr-west-02.cleardb.net',
+        user: 'b059417312a2a9',
+        password: 'a9403b41',
+        database: 'heroku_73fc860b4229c8a'
+    }
+}
